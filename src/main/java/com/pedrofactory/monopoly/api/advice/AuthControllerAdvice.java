2a50241145a11bcc7e3cad4f19da5de43b29808c
Petro Zinchenko <zinchenko.p@dbbest.com>
2020-05-28 18:02:07 +0300

    advice6

--- src/main/java/com/pedrofactory/monopoly/api/advice/AuthControllerAdvice.java
+++ src/main/java/com/pedrofactory/monopoly/api/advice/AuthControllerAdvice.java
@@ -3,6 +3,7 @@ package com.pedrofactory.monopoly.api.advice;
 import com.pedrofactory.monopoly.exception.ClientAlreadyExistsException;
 import com.pedrofactory.monopoly.exception.UserAlreadyExistsException;
 import com.pedrofactory.monopoly.exception.UserNotFoundException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,6 +18,10 @@ public class AuthControllerAdvice {
     }
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<String> userNotFoundException(UserNotFoundException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<String> entityNotExist(EmptyResultDataAccessException ex){
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
