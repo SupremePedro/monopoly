afc2bd59e2566951b3228e39fed7e821dbb37910
Petro Zinchenko <zinchenko.p@dbbest.com>
2020-05-28 17:33:38 +0300

    upgrade advice

--- src/main/java/com/pedrofactory/monopoly/api/advice/AuthControllerAdvice.java
+++ src/main/java/com/pedrofactory/monopoly/api/advice/AuthControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.pedrofactory.monopoly.api.advice;
 
 import com.pedrofactory.monopoly.exception.ClientAlreadyExistsException;
+import com.pedrofactory.monopoly.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,7 +11,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class AuthControllerAdvice {
 
     @ExceptionHandler(ClientAlreadyExistsException.class)
-    public ResponseEntity<String> entityNotFoundResponse(){
+    public ResponseEntity<String> clientAlredyExist(){
         return new ResponseEntity<>("Client identifier already exists", HttpStatus.CONFLICT);
+    }
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> userNotFoundException(UserNotFoundException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
